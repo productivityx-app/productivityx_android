623407d13ccd88633706025421e36680d8e4966c
Chatri Oussama <oussamachatri7@gmail.com>
2026-03-19 16:11:02 +0100

    Fix libs errors

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,10 +13,10 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
