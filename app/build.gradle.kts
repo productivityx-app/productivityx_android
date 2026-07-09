b10c8c2692f062607ed78be8da90f159eea66494
Oussama Chatri <oussamachatri7@gmail.com>
2026-07-09 07:43:53 +0100

    fix: disable crashing lint check and add errorprone keep rules for release build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,10 @@ android {
         }
     }
 
+    lint {
+        disable += "NullSafeMutableLiveData"
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
